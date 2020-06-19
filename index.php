0fc1523e1a0c7010050a659a5c96108e42098bef
zakafahmi <51282976+zakafahmi@users.noreply.github.com>
2020-06-19 13:50:39 +0700

    keterangan
    kalau mau akses php jangan lupa nyalakan apace/mariadb dan juga install php lebih gampang nya istall aja xampp

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>belajar php</title>
+</head>
+<body>
+    <h2>daftar absensi mahasiswa</h2>
+    <ol>
+    <?php 
+    for ($i=1; $i <=10; $i++){
+        echo"<li>nama mahasiswa ke -$i </li>"
+    }
+    ?>
+    </ol>
+</body>
+</html>
